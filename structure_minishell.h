05a7ef5993ff0a7f776d3c2b3757ee108910f102
Julien Kerthe <jkerthe@c2r5p1.42lehavre.fr>
2024-11-20 19:01:16 +0100

    creation de la structure

---
+++ structure_minishell.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structure_minishell.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkerthe <jkerthe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 18:47:45 by jkerthe           #+#    #+#             */
+/*   Updated: 2024/11/20 18:57:16 by jkerthe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTURE_MINISHELL_H
+# define STRUCTURE_MINISHELL_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+typedef s_command
+{
+	s_command	*next;
+	s_task		*first;
+	char		*full_task;
+}			t_command;
+
+typedef s_task
+{
+	s_task	*next;
+	s_task	*prev;
+	char	*content;
+	int		type;
+
+}		t_task;
+
+#endif
