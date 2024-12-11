3dde4c65455735312f353764473a9a27addbe562
v-d-v-d <v@LAPTOP-BAVCELS7>
2024-12-11 17:38:24 +0100

    include main -> minishell.h

---
+++ minishell.h
@@ -0,0 +1,13 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "newlibft/libft.h"
+# include "structure_minishell.h"
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+
+void	free_struct(t_command	*first);
+
+#endif
