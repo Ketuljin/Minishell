892a4f248dfcb8259da398d813456b4aa4b1d287
Vi <152437517+v-d-v-d@users.noreply.github.com>
2024-12-14 03:26:53 +0100

    sturct : bug pach(ok)

--- structure_minishell.h
+++ structure_minishell.h
@@ -3,31 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   structure_minishell.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkerthe <jkerthe@student.42.fr>            +#+  +:+       +#+        */
+/*   By: vdunatte <vdunatte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 18:47:45 by jkerthe           #+#    #+#             */
-/*   Updated: 2024/11/20 18:57:16 by jkerthe          ###   ########.fr       */
+/*   Updated: 2024/12/13 20:59:23 by vdunatte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTURE_MINISHELL_H
 # define STRUCTURE_MINISHELL_H
 
-typedef s_command
+typedef struct s_command
 {
-	s_command	*next;
-	s_task		*first;
-	char		*full_task;
-	int		out_put;
-	int		in_put;
+	struct s_command	*next;
+	struct s_task		*first;
+	char				*full_task;
+	int					out_put;
+	int					in_put;
 }			t_command;
 
-typedef s_task
+typedef struct s_task
 {
-	s_task	*next;
-	s_task	*prev;
-	char	*content;
-	int		type;
+	struct s_task	*next;
+	struct s_task	*prev;
+	char			*content;
+	int				type;
 
 }		t_task;
 
