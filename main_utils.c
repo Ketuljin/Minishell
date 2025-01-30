ffa1a5c039b3fbe6655c85f2e84d454b9c8df496
Vi <152437517+v-d-v-d@users.noreply.github.com>
2025-01-30 02:50:22 +0100

    ~free struct(use ** not *)

--- main_utils.c
+++ main_utils.c
@@ -12,26 +12,29 @@
 
 #include "minishell.h"
 
-void	free_struct2(t_task	*first)
+void	free_struct2(t_task	**first)
 {
-	if (first == NULL)
+	if (*first == NULL)
 		return ;
-	free_struct2(first->next);
-	free(first->content);
-	first->content = NULL;
-	first->prev = NULL;
-	free(first);
-	first = NULL;
+	if ((*first)->next != NULL)
+		free_struct2(&(*first)->next);
+	free((*first)->content);
+	(*first)->content = NULL;
+	free((*first)->prev);
+	(*first)->prev = NULL;
+	free(*first);
+	*first = NULL;
 }
 
-void	free_struct(t_command	*first)
+void	free_struct(t_command	**first)
 {
-	if (first == NULL)
+	if (*first == NULL)
 		return ;
-	free_struct(first->next);
-	free(first->full_task);
-	first->full_task = NULL;
-	free_struct2(first->first);
-	free(first);
-	first = NULL;
+	if ((*first)->next != NULL)
+		free_struct(&(*first)->next);
+	free((*first)->full_task);
+	(*first)->full_task = NULL;
+	free_struct2(&(*first)->first);
+	free(*first);
+	*first = NULL;
 }
