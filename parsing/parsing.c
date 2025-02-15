821e4e1d029f940a0bca3ef0583cd3dc7e68244c
Vi <152437517+v-d-v-d@users.noreply.github.com>
2025-02-15 22:12:53 +0100

    ~split_token(not finish)

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: vdunatte <vdunatte@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 03:53:38 by vdunatte          #+#    #+#             */
-/*   Updated: 2025/02/02 03:11:09 by vdunatte         ###   ########.fr       */
+/*   Updated: 2025/02/14 23:35:38 by vdunatte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,10 @@ int	parsing(char *line, char **envp, t_command **first, int value_return)
 	(void)envp;
 	(void)value_return;
 	temp = NULL;
-	*first = lstnew(NULL);
+	*first = lst_command_new(NULL);
 	i = split_pipe(line, *first);
 	free(line);
+	i = i + split_token(*first);
 	if (i != 0)
 		return (i);
 	free(temp);
--- parsing/split_token.c
+++ parsing/split_token.c
@@ -6,13 +6,13 @@
 /*   By: vdunatte <vdunatte@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 23:56:44 by vdunatte          #+#    #+#             */
-/*   Updated: 2025/02/08 01:35:19 by vdunatte         ###   ########.fr       */
+/*   Updated: 2025/02/15 00:50:02 by vdunatte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-t_task	*lst_task_new(void *content, t_task prev)
+t_task	*lst_task_new(void *content, t_task *prev)
 {
 	t_task	*lst;
 
@@ -26,7 +26,45 @@ t_task	*lst_task_new(void *content, t_task prev)
 	return (lst);
 }
 
+int	splitter(char *full_task, t_task *first)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	temp = full_task;
+	while (temp[0] != '\0')
+	{
+		if (skip_quote(&temp) == 1)
+			return (1);
+		if (temp[0] == ' ' || temp[0] == '	')
+		{
+			i = (long)temp - (long)full_task;
+			first->content = extract_skip(&full_task, i);
+			if (first->content == NULL)
+				return (1);
+			ft_lstadd_back((t_list **)&first, (t_list *)lst_task_new(NULL, NULL));
+			printf("2 %s\n", first->content);
+			first = first->next;
+			temp = ++full_task;
+		}
+		if (temp[0] != '\0')
+			temp++;
+	}
+	first->content = ft_strdup(full_task);
+	printf("3 %s\n", first->content);
+	if (first->content == NULL)
+		return (1);
+	return (0);
+}
+
 int	split_token(t_command *first)
 {
-	
+	if (first == NULL)
+		return (0);
+	first->first = lst_task_new(NULL, NULL);
+	printf("1 %s\n\n", first->full_task);
+	if (splitter(first->full_task, first->first) == 1)
+		return (1);
+	return (split_token(first->next));
 }
