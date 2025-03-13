095a1c82ae3b6c7f6add51ca827b4b89155dc00f
Vi <152437517+v-d-v-d@users.noreply.github.com>
2025-03-13 20:51:16 +0100

    include

---
+++ include/minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdunatte <vdunatte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 20:38:37 by vdunatte          #+#    #+#             */
+/*   Updated: 2024/12/13 20:38:37 by vdunatte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+
+# include "../newlibft/libft.h"
+# include "parsing.h"
+# include "structure_minishell.h"
+# include "structure_execute.h"
+
+void	free_struct(t_command **first);
+
+#endif
---
+++ include/structure_minishell.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structure_minishell.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdunatte <vdunatte@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 18:47:45 by jkerthe           #+#    #+#             */
+/*   Updated: 2025/03/04 03:28:58 by vdunatte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTURE_MINISHELL_H
+# define STRUCTURE_MINISHELL_H
+
+typedef struct s_command
+{
+	struct s_command	*next;
+	struct s_task		*first;
+	char				*full_task;
+	int					fd_in_put;
+	int					fd_out_put;
+}			t_command;
+
+typedef struct s_task
+{
+	struct s_task	*next;
+	char			*content;
+	int				type;
+}		t_task;
+
+typedef struct s_env_ex
+{
+	char	**env;
+	int		exit_code;
+}		t_env_ex;
+
+#endif
