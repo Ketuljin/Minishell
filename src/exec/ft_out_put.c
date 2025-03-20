eb59fb9d4619a5fba32d88d6ba1e5f9ceb3ab287
julienKerthe <kerthejulien@gmail.com>
2025-03-20 11:51:06 +0100

    STDOUT issues resolve

--- src/exec/ft_out_put.c
+++ src/exec/ft_out_put.c
@@ -71,7 +71,7 @@ void	ft_close_out_put(t_command *command, int fd_save)
 		return ;
 	}
 	close(fd_save);
-	if (command->fd_out_put > 0)
+	if (command->fd_out_put > 2)
 		close(command->fd_out_put);
-	command->fd_out_put = STDERR_FILENO;
+	command->fd_out_put = STDOUT_FILENO;
 }
