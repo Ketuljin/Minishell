cf7be5daae434def149a4136f5645c38f20cad22
Vi <152437517+v-d-v-d@users.noreply.github.com>
2025-03-21 21:21:58 +0100

    norm

--- src/parsing/trans_var.c
+++ src/parsing/trans_var.c
@@ -76,7 +76,7 @@ int	trans_var(t_task **token, t_env_ex **env_ex, char **newcnt, t_count **count)
 	int		k;
 
 	if (ft_isalnum((*token)->content[++(*count)->i]) == 0
-		&& ft_strchr("_?",(*token)->content[(*count)->i]) == NULL)
+		&& ft_strchr("_?", (*token)->content[(*count)->i]) == NULL)
 		return (end_line(newcnt, count));
 	temp = extract_var(((*token)->content), &(*count)->i);
 	if ((*token)->content[(*count)->i] == '?')
